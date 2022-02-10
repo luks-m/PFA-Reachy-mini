ca517fc8fa1102497e063812d06d538d695e93c4
Aymane <79176510+Aymdi@users.noreply.github.com>
2022-02-10 14:26:56 +0100

    up speech synthesis using gTTS API

---
+++ speech_synthesis_gtt.py
@@ -0,0 +1,12 @@
+'''
+Speech synthesis using gTTS API
+'''
+
+from gtts import gTTS
+from io import BytesIO
+import os
+
+mp3_fp = BytesIO()
+tts = gTTS(text='salut, je suis reachy ! Vous voulez une photo ?', lang='fr')
+tts.write_to_fp(mp3_fp)
+os.system("mpg123 file.mp3")
