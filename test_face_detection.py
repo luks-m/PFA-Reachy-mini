9bb923439b202e9cbcb06b1b5154114ab233a20b
Sylvain Raïs <sylvain.rais24@gmail.com>
2022-02-20 18:28:07 +0100

    Test des deux fonctions d'interface pour la détection faciale réussi en environnement NON REACHY. Documentation de toutes les fonctions pertinentes. Ajout de la classe Reachy_camera offrant une abstraction quant aux services liés directement à la caméra. Ajout d'un script sh pour tuer tout programme python3 tourannt en fond

--- test_face_detection.py
+++ test_face_detection.py
@@ -3,7 +3,9 @@ from face_detection import *
 
 
 # Test functions
-def run_all_face_tests(get_frame, with_angle_to_center):
+
+## For first MVP
+def run_all_face_tests(get_frame, with_angle_to_center):    # Code factorization function to identify all faces without interface functions
     while True:
         frame = get_frame()
         
@@ -35,25 +37,24 @@ def test_all_face_recognised(with_angle_to_center): # Not working on Reachy
     free_capture_and_windows(cap)
 
 def test_all_face_recognised_with_reachy_api(with_angle_to_center):
-    reachy = ReachySDK(host='localhost')  # Replace with the actual IP
-    camera = reachy.left_camera
+    camera = initiate_reachy_camera(ReachySDK(host='localhost'))  # Replace with the actual IP
 
     def get_frame():
-        frame = camera.last_frame
-        return give_in_gray(frame)
+        return give_in_gray(camera.get_frame())
 
     run_all_face_tests(get_frame, with_angle_to_center)       
 
-def run_get_angle_functions_tests(get_frame, get_angle, get_angle_args):
+
+## For interface functions
+def run_get_angle_functions_tests(get_frame, get_angle, get_angle_args):    # Code factorization function to print the average angle given by the get_angle function
     while True:
         frame = get_frame()
         
-        angle = get_angle(frame, get_angle_args)
+        angle = get_angle(frame, get_angle_args, for_test = True)
         
         print(f"horizontal angle is {round(angle.h, 1)}° - vertical angle is {round(angle.v, 1)}°")
 
         time.sleep(0.1)
-        frame_display(frame, 'face_detection')
         if cv2.waitKey(1) == ord('q'):
             break
 
@@ -70,7 +71,48 @@ def test_n_closest_angle(n): # Not working on Reachy
     run_get_angle_functions_tests(get_frame, n_closest_angle, n)    
     free_capture_and_windows(cap)
 
-if __name__ == '__main__':
-    # test_all_face_recognised(True)
+def test_n_closest_angle_with_reachy_api(n):
+    camera = initiate_reachy_camera(ReachySDK(host='localhost'))  # Replace with the actual IP
+
+    def get_frame():
+        return camera.get_frame()
+
+    run_get_angle_functions_tests(get_frame, n_closest_angle, n)
+
+def test_framing_for_group_photo_angle(percent_relat_to_avg): # Not working on Reachy
+    cap = open_capture(0)
+
+    def get_frame():
+        success, frame = read_capture(cap)
+        if not success:
+            print("Can't receive frame (stream end?). Exiting ...")
+            exit()
+        return frame
+
+    run_get_angle_functions_tests(get_frame, framing_for_group_photo_angle, percent_relat_to_avg)    
+    free_capture_and_windows(cap)
+
+def test_framing_for_group_photo_angle__with_reachy_api(percent_relat_to_avg):
+    camera = initiate_reachy_camera(ReachySDK(host='localhost'))  # Replace with the actual IP
+
+    def get_frame():
+        return camera.get_frame()
+
+    run_get_angle_functions_tests(get_frame, framing_for_group_photo_angle, percent_relat_to_avg)
+
+
+if __name__ == '__main__': # Relevant test experiences
+    test_all_face_recognised(True)
     # test_all_face_recognised_with_reachy_api(False)
-    test_n_closest_angle(2)
+
+    # test_n_closest_angle(0)
+    # test_n_closest_angle(1)
+    # test_n_closest_angle(2)
+    # test_n_closest_angle_with_reachy_api(0)
+    # test_n_closest_angle_with_reachy_api(1)
+    # test_n_closest_angle_with_reachy_api(2)
+
+    # test_framing_for_group_photo_angle(0.2)
+    # test_framing_for_group_photo_angle(0.5)
+    # test_framing_for_group_photo_angle__with_reachy_api(0.2)
+    # test_framing_for_group_photo_angle__with_reachy_api(0.5)
