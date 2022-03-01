937093c43058f39967013709181cdd624454c54a
BOUDEAU Benjamin <Benjamin.Boudeau@bordeaux-inp.fr>
2022-03-01 16:08:39 +0100

    correction du fichier de commandes de conversation car pas besoin de '?', factorisation du code de conversation

--- vocal_recognition.py
+++ vocal_recognition.py
@@ -2,6 +2,8 @@ import speech_recognition as sr
 from scipy.io import wavfile
 import noisereduce as nr 
 
+WAV_OUTPUT_FILENAME = "enregistrement.wav"
+WAV_OUTPUT_REDUCED_FILENAME = "enregistrement_reduce_noise.wav"
 
 def debug_print(str):
     print("DEBUG : " + str)
@@ -14,17 +16,17 @@ def debug_print(str):
 def record_and_transcript():
     r = sr.Recognizer()
     with sr.Microphone() as source:
-        r.adjust_for_ambient_noise(source, duration=0.5) #  
+        r.adjust_for_ambient_noise(source, duration=2) #  
         debug_print('Parle !')
-        audio = r.listen(source)
-        with open("enregistrement.wav", "wb") as f:
+        audio = r.listen(source,phrase_time_limit=5)
+        with open(WAV_OUTPUT_FILENAME, "wb") as f:
             f.write(audio.get_wav_data())
         debug_print('Fin !')
     try:
-        rate, data = wavfile.read("enregistrement.wav")
+        rate, data = wavfile.read(WAV_OUTPUT_FILENAME)
         reduced_noise = nr.reduce_noise(y=data, sr=rate)
-        wavfile.write("enregistrement_reduce_noise.wav", rate, reduced_noise)
-        enregistrement_r_n = sr.AudioFile("enregistrement_reduce_noise.wav")
+        wavfile.write(WAV_OUTPUT_REDUCED_FILENAME, rate, reduced_noise)
+        enregistrement_r_n = sr.AudioFile(WAV_OUTPUT_REDUCED_FILENAME)
         with enregistrement_r_n as source:
             better_audio = r.record(source)
         query = r.recognize_google(better_audio, language = 'fr-FR')
