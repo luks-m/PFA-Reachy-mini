92559231ea9e40fd1230c6284570085de533b495
luks-m <lmarais02@outlook.fr>
2022-03-03 13:05:47 +0100

    up playvoice.py

--- voices/playvoice.py
+++ voices/playvoice.py
@@ -2,9 +2,10 @@ from playsound import playsound
 import random
 import os
 
-list = os.listdir('.')
-list.remove("playvoice.py")
-#list.remove("playsound.py~")
+list_voice = os.listdir('.')
+list_voice = [k for k in list_voice if '.mp3' in k]
+#list_voice.remove("playvoice.py")
+#list_voice.remove("playvoice.py~")
 
-voice = random.choice(list)
+voice = random.choice(list_voice)
 playsound(voice)
