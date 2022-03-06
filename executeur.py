aea218de717e66fbff6d66c6d64aa5154508f018
BOUDEAU Benjamin <Benjamin.Boudeau@bordeaux-inp.fr>
2022-03-06 11:16:08 +0100

    correction de executeur.py (valeur de initial_state)

--- executeur.py
+++ executeur.py
@@ -7,7 +7,8 @@ class Executeur :
     def __execute_once(self, state, context = {}):
         return self.state_machine.call_state(state, context)
 
-    def execute(self, initial_state, initial_context = {}, iteration = 0):
+    def execute(self, initial_context = {}, iteration = 0):
+        initial_state = self.state_machine.get_initial_state()
         state, context = self.__execute_once(initial_state, initial_context)
         if(iteration == 0):
             while(True):
