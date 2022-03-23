657954c1cd20108337cfdb46a8ce810372bc0ebf
LAMHAMDI Aymane <Aymane.Lamhamdi@bordeaux-inp.fr>
2022-03-23 09:59:38 +0100

    upgrading speech synthesis using gTTS

--- src/speech_synthesis_gtt.py
+++ src/speech_synthesis_gtt.py
@@ -6,7 +6,17 @@ from gtts import gTTS
 from io import BytesIO
 import os
 
-mp3_fp = BytesIO()
-tts = gTTS(text='salut, je suis reachy ! Vous voulez une photo ?', lang='fr')
-tts.write_to_fp(mp3_fp)
-os.system("mpg123 ./tmp/file.mp3")
+PATH = "./tmp/"
+
+def text_to_speech_gtt(text):
+
+    mp3_fp = BytesIO()
+    tts = gTTS(text, lang='fr')
+    tts.write_to_fp(mp3_fp)
+    os.system("mpg123 ./tmp/file.mp3")
+
+    if os.path.exists(PATH+"file.mp3"):
+        os.remove(PATH+"file.mp3")     #removing the mp3 file
+
+
+text_to_speech_gtt('salut, je suis reachy ! Vous voulez une photo ?')
