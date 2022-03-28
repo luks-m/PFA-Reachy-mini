c596bd2fed776997ca5bac6b4d49d744d7857c1d
LAMHAMDI Aymane <Aymane.Lamhamdi@bordeaux-inp.fr>
2022-03-28 11:16:30 +0200

    up speech_synthesis

--- src/speech_synthesis_gtt.py
+++ src/speech_synthesis_gtt.py
@@ -8,16 +8,16 @@ import os
 
 PATH = "./tmp/"
 
-def text_to_speech_gtt(text):
+def text_to_speech_gtt(qlq):
 
     mp3_fp = BytesIO()
-    tts = gTTS(text, lang='fr')
-    tts.write_to_fp(mp3_fp)
-    os.system("mpg123 ./tmp/file.mp3")
+    tts = gTTS(text=qlq, lang='fr')
+    tts.save(mp3_fp)
+    #os.system("mpg123 ./tmp/file.mp3")
 
-    if os.path.exists(PATH+"file.mp3"):
-        os.remove(PATH+"file.mp3")     #removing the mp3 file
+    #if os.path.exists(PATH+"file.mp3"):
+     #   os.remove(PATH+"file.mp3")     #removing the mp3 file
 
 if __name__ == '__main__':
-
+    
     text_to_speech_gtt('salut, je suis reachy ! Vous voulez une photo ?')
