587b6e42ba294120548589bfbb362dd1ada4f170
Sylvain <sylvain.rais24@gmail.com>
2022-04-07 15:40:16 +0200

    Creation of a function in initiate_camera for a possible future use

--- src/detection/initiate_camera.py
+++ src/detection/initiate_camera.py
@@ -3,15 +3,18 @@ sys.path.append("../session")
 from session import *
 
 # To set the focus
+def set_focus():
+    session = ReachySession()
+    session.start_autofocus()
 
-session = ReachySession()
-session.start_autofocus()
+    while True:
+            frame = get_frame(session)
+            # time.sleep(0.1)
+            frame_display(frame, 'focus_setting_window')
+            if cv2.waitKey(1) == ord('q'):
+                break
 
-while True:
-        frame = get_frame(session)
-        # time.sleep(0.1)
-        frame_display(frame, 'focus_setting_window')
-        if cv2.waitKey(1) == ord('q'):
-            break
+    session.stop_autofocus()
 
-session.stop_autofocus()
+if __name__ == '__main__':
+    set_focus()
