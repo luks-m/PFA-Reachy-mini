715e2ab28857469502d89106d2f9898504025d94
MARAIS Lucas <Lucas.Marais@bordeaux-inp.fr>
2022-04-14 11:42:00 +0200

    comment in movement.py

--- src/movement.py
+++ src/movement.py
@@ -5,15 +5,16 @@ import sys
 sys.path.append("../session")
 from session import *
 
-
+#Switch on the motorisation of the head
 def motor_on(session):
     session.turn_on()
 
+#Switch off the motorisation of the head
 def motor_off(session):
     session.turn_off()
 
+#Convert euler angles into quaternion
 def __euler_to_quaternion(roll, pitch, yaw):
-
     qx = np.sin(roll/2) * np.cos(pitch/2) * np.cos(yaw/2) - np.cos(roll/2) * np.sin(pitch/2) * np.sin(yaw/2)
     qy = np.cos(roll/2) * np.sin(pitch/2) * np.cos(yaw/2) + np.sin(roll/2) * np.cos(pitch/2) * np.sin(yaw/2)
     qz = np.cos(roll/2) * np.cos(pitch/2) * np.sin(yaw/2) - np.sin(roll/2) * np.sin(pitch/2) * np.cos(yaw/2)
@@ -21,9 +22,11 @@ def __euler_to_quaternion(roll, pitch, yaw):
 
     return [qx, qy, qz, qw]
 
+#Convert degree angles into radian
 def __degree_to_radian(theta):
     return theta*2*np.pi / 360
 
+#Check the validity of the angles and correct them if necessary
 def __fit_angles(theta, phi):
     if 45 < theta and theta < 130:
         t = theta
@@ -39,6 +42,7 @@ def __fit_angles(theta, phi):
         p = -45
     return t, p
 
+#Convert spherical cooridnates into cartesian
 def __spherical_to_cartesian(radius, theta, phi):
     theta = __degree_to_radian(theta)
     phi = __degree_to_radian(phi)
@@ -47,6 +51,7 @@ def __spherical_to_cartesian(radius, theta, phi):
     z = round(radius*np.cos(theta), 2)
     return [x, y, z]
 
+#Compute the duration of the move according to the velocity and the previous and next positions
 def __duration(coordinates_prev, coordinates_next, v):
     duration = round(np.sqrt((coordinates_next[0] - coordinates_prev[0])**2 + (coordinates_next[1]-coordinates_prev[1])**2 + (coordinates_next[2]-coordinates_prev[2])**2)/v, 2)
     if duration <= 0:
@@ -54,12 +59,14 @@ def __duration(coordinates_prev, coordinates_next, v):
     else:
         return duration
 
+#Make the head move to the given position without storing the previous position (emotional move)
 def move_to(session, radius, theta, phi, v):
     position_prev = __spherical_to_cartesian(0.5, session.TMP_THETA, session.TMP_PHI)
     session.TMP_THETA, session.TMP_PHI = __fit_angles(theta, phi)
     position = __spherical_to_cartesian(radius, session.TMP_THETA, session.TMP_PHI)
     session.look_at(position[0], position[1], position[2], __duration(position_prev, position, v))
 
+#Make the head move and store the previous position to move back if necessary
 def update_position(session, theta, phi, v):
     print(theta, phi)
     theta, phi = round(theta,2), round(phi,2)
@@ -87,12 +94,14 @@ def update_position(session, theta, phi, v):
     session.THETA = session.TMP_THETA
     session.PHI = session.TMP_PHI
 
+#Make the emotion of listening
 def listen(session):
     session.r_antenna_set_position(0)
     session.l_antenna_set_position(0)
 
     move_to(session, 0.5, session.THETA, session.PHI, 0.15)
         
+#Make the emotion of being sad
 def sad(session):
     session.r_antenna_set_position(70.0)
     session.l_antenna_set_position(70.0)
@@ -102,6 +111,7 @@ def sad(session):
 
     move_to(session, 0.5, 31.15 + session.THETA, session.PHI, 0.13)
 
+#Make the emotion of being happy
 def happy(session):
     move_to(session, 0.5, 5.74 + session.THETA, session.PHI, 0.15)
 
@@ -118,6 +128,7 @@ def happy(session):
     session.r_antenna_set_position(0.0)
     session.l_antenna_set_position(0.0)
 
+#Make the move to attract people
 def incentive(session):
     session.antennas_speed_limit(70.0)
     session.r_antenna_set_position(-35.0)
@@ -125,6 +136,7 @@ def incentive(session):
 
     move_to(session, 0.5, -5.74 + session.THETA, session.PHI, 0.1)
 
+#Make the move of thinking
 def thinking(session):
     session.antennas_speed_limit(70.0)
     session.r_antenna_set_position(40.0)
@@ -132,6 +144,7 @@ def thinking(session):
 
     move_to(session, 0.5, -16.13 + session.THETA, 16.7 + session.PHI, 0.21)
 
+#Make the move to thank people
 def thanking(session):
     session.antennas_speed_limit(70.0)
     session.r_antenna_set_position(0.0)
@@ -153,6 +166,7 @@ def thanking(session):
     
     move_to(session, 0.5, 5.74 + session.THETA, session.PHI, 0.35)
 
+#move back to the previous position
 def move_back(session):
     position_prev = __spherical_to_cartesian(0.5, session.TMP_THETA, session.TMP_PHI)
     session.TMP_THETA = session.THETA
