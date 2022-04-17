84b595f52a591484a85a2a443fd9714affd2cd92
luks-m <lmarais02@outlook.fr>
2022-04-17 17:06:18 +0200

    ok

--- src/state_machines/reachy_state_machine_sans_recherche.py
+++ src/state_machines/reachy_state_machine_sans_recherche.py
@@ -3,7 +3,6 @@ import sys
 sys.path.append("../session")
 sys.path.append("..")
 import reachy_session as rs
-import fake_session as fs
 
 ###################################
 ## Creation of the State Machine ##
@@ -17,11 +16,7 @@ state_machine = ld.load("../../assets/json/state_machine_sans_recherche.json")
 ## Test ##
 ##########
 
-S = rs.ReachySession()
-#F = fs.FakeSession()
 
-executeur = state_machine.create_executor(S)
-#executeur = state_machine.create_executor(F)
-
+executeur = state_machine.create_executor(rs.ReachySession())
 
 executeur.launch()
