1fb5c9082de6a7d2a46971b0179585509b01ff57
Widad <widadzizouan16@gmail.com>
2022-04-28 17:13:11 +0200

    comments

--- src/advanced_conversation.py
+++ src/advanced_conversation.py
@@ -7,6 +7,27 @@ sys.path.append("../recognition")
 import speech_synthesis_gtt as speech
 import vocal_recognition as vr
 
+'''
+
+#This is a function that takes a sentence from the user as input
+#and returns the response of Reachy based on openai
+
+Here are some informations about the parameteres used:
+
+    #engine="text-davinci-002": the engine or the model that will generate the completion "the response"
+    #prompt: the sentence that the user will be prompted to say
+    #temperature: this parameter is used to control the diversity and randomness of the response,
+                the higher the temperature, the more diverse the response.
+    #max_tokens: the maximum number of tokens that the response can have
+    #best_of: the number of best responses that will be returned
+    #top_p: the probability threshold that will be used to filter the responses
+    #frequency_penalty: the penalty that will be applied to the responses that are too frequent,
+                  it decreases the probability of the responses that are too frequent
+    #presence_penalty: the penalty that will be applied to the responses that are too short,
+                  it increases the likelihood of the model to talk about new topics
+
+'''
+
 def openai_speech(str):
   
   openai.api_key="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"
@@ -22,7 +43,7 @@ def openai_speech(str):
     prompt=dialog ,
     temperature=0.9,
     max_tokens=50,
-    best_of=4,
+    best_of=1,
     top_p=1,
     frequency_penalty=0.0,
     presence_penalty=0.6,
